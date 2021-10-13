c386e36b1a319b333909cf79ea76d5335e2f45fe
Sujay Chand <sujaygchand@gmail.com>
2021-10-14 01:22:21 +1300

    Made unit tests for push for Stack

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+	[TestFixture]
+	public class StackTests
+	{
+		private Fundamentals.Stack<int?> _intStack;
+		private Fundamentals.Stack<string> _stringStack;
+
+		private static readonly object[] _intSourceLists =
+		{
+		new object[] {new List<int?> {1}},   //case 1
+		new object[] {new List<int?> {1, 2}}, //case 2
+		new object[] {new List<int?> {1, 3, 4, 5, 6}}, //case 3
+		new object[] {new List<int?> { 1, 3, 4, 5, 7, 34, 6, 11 } } //case 4
+		};
+
+		private static readonly object[] _stringSourceLists =
+{
+		new object[] {new List<string> {"1"}},   //case 1
+		new object[] {new List<string> {"1", "2"}}, //case 2
+		new object[] {new List<string> {"one", "two" ,"Three", "four"}}, //case 3
+		new object[] {new List<string> { "one", "two", "Three", "four", "five", "six", "seven"} } //case 4
+		};
+
+		[SetUp]
+		public void Setup()
+		{
+			_intStack = new Fundamentals.Stack<int?>();
+			_stringStack = new Fundamentals.Stack<string>();
+		}
+
+		[Test]
+		[TestCaseSource("_intSourceLists")]
+		public void Push_WhenIntStackCalled_AddElementsToList(List<int?> inputList)
+		{
+			foreach (var item in inputList)
+			{
+				_intStack.Push(item);
+			}
+
+			Assert.That(_intStack.List, Is.EquivalentTo(inputList));
+		}
+
+		[Test]
+		[TestCaseSource("_stringSourceLists")]
+		public void Push_WhenStringStackCalled_AddElementsToList(List<string> inputList)
+		{
+			foreach (var item in inputList)
+			{
+				_stringStack.Push(item);
+			}
+
+			Assert.That(_stringStack.List, Is.EquivalentTo(inputList));
+		}
+	}
+}
--- TestNinja/Fundamentals/Stack.cs
+++ TestNinja/Fundamentals/Stack.cs
@@ -3,38 +3,43 @@ using System.Collections.Generic;
 
 namespace TestNinja.Fundamentals
 {
-    public class Stack<T>
-    {
-        private readonly List<T> _list = new List<T>();
+	public class Stack<T>
+	{
+		private readonly List<T> _list = new List<T>();
 
-        public int Count => _list.Count;
+		public List<T> List
+		{
+			get { return _list; }
+		}
 
-        public void Push(T obj)
-        {
-            if (obj == null)
-                throw new ArgumentNullException();
-            
-            _list.Add(obj);
-        }
+		public int Count => _list.Count;
 
-        public T Pop()
-        {
-            if (_list.Count == 0)
-                throw new InvalidOperationException();
+		public void Push(T obj)
+		{
+			if (obj == null)
+				throw new ArgumentNullException();
 
-            var result = _list[_list.Count - 1];
-            _list.RemoveAt(_list.Count - 1);
+			_list.Add(obj);
+		}
 
-            return result; 
-        }
+		public T Pop()
+		{
+			if (_list.Count == 0)
+				throw new InvalidOperationException();
+
+			var result = _list[_list.Count - 1];
+			_list.RemoveAt(_list.Count - 1);
+
+			return result;
+		}
 
 
-        public T Peek()
-        {
-            if (_list.Count == 0)
-                throw new InvalidOperationException();
+		public T Peek()
+		{
+			if (_list.Count == 0)
+				throw new InvalidOperationException();
 
-            return _list[_list.Count - 1];
-        }
-    }
+			return _list[_list.Count - 1];
+		}
+	}
 }
