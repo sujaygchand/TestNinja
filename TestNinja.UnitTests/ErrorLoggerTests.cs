cba1fcd6afc14b7cbf2defae3e2cf92f05e72d29
Sujay Chand <sujaygchand@gmail.com>
2021-10-13 23:18:06 +1300

    Test thrown exceptions

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -7,15 +7,32 @@ namespace TestNinja.UnitTests
 	[TestFixture]
 	public class ErrorLoggerTests
 	{
+		ErrorLogger _logger;
+
+		[SetUp]
+		public void Setup()
+		{
+			_logger = new ErrorLogger();
+		}
+
 		[Test]
 		public void Log_WhenCalled_SetTheLastErrorProperty()
 		{
-			var logger = new ErrorLogger();
 			var input = "Test text";
 
-			logger.Log(input);
+			_logger.Log(input);
 
-			Assert.That(logger.LastError, Is.EqualTo(input));
+			Assert.That(_logger.LastError, Is.EqualTo(input));
 		}
+
+		[Test]
+		[TestCase(null)]
+		[TestCase("")]
+		[TestCase(" ")]
+		public void Log_InvalidError_SetTheLastErrorProperty(string error)
+		{
+			Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+		}
+
 	}
 }
