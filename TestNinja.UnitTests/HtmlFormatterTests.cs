c21b59b6fb3d116dc52060e3550d90d0ecff5933
Sujay Chand <sujaygchand@gmail.com>
2021-10-13 22:21:02 +1300

    Testing cases that return strings

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+	[TestFixture]
+	public class HtmlFormatterTests
+	{
+		[Test]
+		public void FormatAsBold_WhenCalled_ShouldEncloseWithStringElement()
+		{
+			var formatter = new HtmlFormatter();
+			string input = "abcde";
+
+			var result = formatter.FormatAsBold(input);
+
+			Assert.That(result, Is.EqualTo($"<strong>{input}</strong>").IgnoreCase);
+		}
+	}
+}
