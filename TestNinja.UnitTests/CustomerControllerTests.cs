5863a2d45cd65e37ad7485e4f321affb8dd9151d
Sujay Chand <sujaygchand@gmail.com>
2021-10-13 22:48:48 +1300

    Testing methods with return types

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+	[TestFixture]
+	public class CustomerControllerTests
+	{
+		CustomerController _controller;
+
+		[SetUp]
+		public void Setup()
+		{
+			_controller = new CustomerController();
+		}
+
+		[Test]
+		[TestCase(0)]
+		[TestCase(-1)]
+		public void GetCustomer_IdLessThanOne_ReturnNotFound(int id)
+		{
+			var controller = new CustomerController();
+
+			var result = controller.GetCustomer(id);
+
+			Assert.That(result, Is.TypeOf<NotFound>());
+		}
+
+		[Test]
+		[TestCase(1)]
+		[TestCase(9)]
+		[TestCase(3009)]
+		public void GetCustomer_IdGreaterThanZero_ReturnOk(int id)
+		{
+			var controller = new CustomerController();
+
+			var result = controller.GetCustomer(id);
+
+			Assert.That(result, Is.TypeOf<Ok>());
+		}
+	}
+}
--- TestNinja/Fundamentals/CustomerController.cs
+++ TestNinja/Fundamentals/CustomerController.cs
@@ -4,7 +4,7 @@
     {
         public ActionResult GetCustomer(int id)
         {
-            if (id == 0)
+            if (id <= 0)
                 return new NotFound();
             
             return new Ok();
