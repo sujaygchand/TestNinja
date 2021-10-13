bf217114439c2284c78e97430448759de6368f35
Sujay Chand <sujaygchand@gmail.com>
2021-10-13 23:02:34 +1300

    Testing void logic

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+	[TestFixture]
+	public class ErrorLoggerTests
+	{
+		[Test]
+		public void Log_WhenCalled_SetTheLastErrorProperty()
+		{
+			var logger = new ErrorLogger();
+			var input = "Test text";
+
+			logger.Log(input);
+
+			Assert.That(logger.LastError, Is.EqualTo(input));
+		}
+	}
+}
